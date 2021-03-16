518b911e7718ed19c45d598ed87ea9d79d5d8f40
Hadeer <mhadeer95@yahoo.com>
2021-03-16 11:28:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testnavigationcoponent/TitleFragment.kt
@@ -0,0 +1,53 @@
+package com.example.testnavigationcoponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_title.*
+
+
+class TitleFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //this way
+       /* goToSecondDestination.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }*/
+
+        //second way using KTX
+  /*       goToSecondDestination.setOnClickListener {
+         it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+           }*/
+
+
+        //third way using onclick listener of navigation component
+        goToSecondDestination.setOnClickListener (
+         Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
+
+    }
+
+
+}
